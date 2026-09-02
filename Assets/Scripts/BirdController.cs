4dcbc44a1ec86205192c6e95dcfa192e90699d32
SimoGD <tom.ouwehand.r@gmail.com>
2026-09-02 23:03:36 +0200

    bird cannot leave the camera's view

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -1,5 +1,7 @@
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.GraphicsBuffer;
 
 public class BirdController : MonoBehaviour
 {
@@ -10,7 +12,6 @@ public class BirdController : MonoBehaviour
     private bool _isAscendPressed;
     private bool _isSteerPressed;
     private float _direction;
-    private Vector3 _velocity;
     
     void OnAscend()
     {
@@ -26,8 +27,13 @@ public class BirdController : MonoBehaviour
     void Awake()
     {
         _rigidbody = GetComponentInChildren<Rigidbody>();
+
         _isAscendPressed = false;
         _isSteerPressed = false;
+    }
+    void LateUpdate()
+    {
+        ClampPlayerMovement();
     }
 
     private void FixedUpdate()
@@ -42,14 +48,38 @@ public class BirdController : MonoBehaviour
 
         if (_isAscendPressed)
         {
-            newVelocity = new Vector3(0, _jumpForce, 0);
+            newVelocity = new Vector3(_rigidbody.linearVelocity.x, _jumpForce, 0);
             _isAscendPressed = false;
         }
 
         if (newVelocity != Vector3.zero)
         {
+            _rigidbody.linearVelocity = new Vector3(0, _rigidbody.linearVelocity.y, 0);
             _rigidbody.AddForce(newVelocity, ForceMode.Impulse);
-            _velocity = newVelocity;
         }
+    }
+
+
+    // bron: https://stackoverflow.com/questions/42800645/how-to-completely-prevent-the-player-from-going-offscreen-in-unity
+    private void ClampPlayerMovement()
+    {
+        Vector3 position = transform.position;
+
+        float distance = transform.position.z - Camera.main.transform.position.z;
+
+        float leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance)).x;
+        float rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance)).x;
+
+        float ceiling = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, distance)).y;
+
+        if (ceiling < transform.position.y)
+        {
+            _rigidbody.linearVelocity = new Vector3(_rigidbody.linearVelocity.x, 0, 0);
+        }
+
+        position.x = Mathf.Clamp(position.x, leftBorder, rightBorder);
+        position.y = Mathf.Clamp(position.y, -4, ceiling);
+
+        transform.position = position;
     }
 }
